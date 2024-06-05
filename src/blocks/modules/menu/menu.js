db0bc435b97e4a87916c148127b8f8c77f8342e3
Liza Elkina <lizapetkova@yandex.ru>
2024-06-05 16:20:05 +0300

    feature: mobile menu js

--- src/blocks/modules/menu/menu.js
+++ src/blocks/modules/menu/menu.js
@@ -1,1 +1,37 @@
+const page = document.querySelector('.page');
+const menu = page.querySelector('.menu');
+const overlay = page.querySelector('.overlay');
+const buttonOpenMenu = page.querySelector('.header__burger');
+const buttonCloseMenu = menu.querySelector('.menu__button');
+const anchorMenu = menu.querySelectorAll('.menu__link');
 
+function openMenu() {
+  menu.classList.add('menu_opened');
+  overlay.classList.add('overlay__menu_opened');
+  document.addEventListener('keydown', closeMenuByEsc);
+}
+
+function closeMenu() {
+  menu.classList.remove('menu_opened');
+  overlay.classList.remove('overlay__menu_opened');
+  document.addEventListener('keydown', closeMenuByEsc);
+}
+
+function closeMenuByEsc(event) {
+  if(event.key === 'Escape') {
+    menu.querySelector('.menu_opened');
+    closeMenu();
+  }
+}
+
+buttonOpenMenu.addEventListener('click', openMenu);
+
+buttonCloseMenu.addEventListener('click', closeMenu);
+
+menu.addEventListener('mousedown', (event) => {
+  if (event.target === menu) {
+    closeMenu(menu);
+  }
+});
+
+anchorMenu.forEach(link => link.addEventListener('click', () => closeMenu()));
