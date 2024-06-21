a394602a3d7770e34761a203824d1339dc890f70
Liza Elkina <lizapetkova@yandex.ru>
2024-06-21 15:14:29 +0300

    fix: modal-video close

--- src/blocks/modules/modal/modal-video/modal-video.js
+++ src/blocks/modules/modal/modal-video/modal-video.js
@@ -1,9 +1,10 @@
-import {openModal, closeModal} from '%modules%/modal/modal';
+import {openModal} from '%modules%/modal/modal';
 
 const page = document.querySelector('.page');
 const modalVideo = page.querySelector('.modal__video');
 const modalContent = modalVideo.querySelector('.modal__content');
 const buttonOpenVideoModal = page.querySelector('.about__play');
+const buttonCloseVideoModal = modalVideo.querySelector('.modal__button');
 
 function openVideoModal() {
   modalContent.innerHTML =
@@ -18,10 +19,10 @@ function openVideoModal() {
   openModal(modalVideo);
 }
 
-// eslint-disable-next-line no-unused-vars
 function closeVideoModal() {
   modalContent.innerHTML = '';
-  closeModal(modalVideo);
 }
 
 buttonOpenVideoModal.addEventListener('click', openVideoModal);
+
+buttonCloseVideoModal.addEventListener('click', closeVideoModal);
