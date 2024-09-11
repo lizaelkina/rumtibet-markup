c03eceb28c676942d7d4e4a43c3d6282edd2a13c
Liza Elkina <lizapetkova@yandex.ru>
2024-09-11 18:21:42 +0300

    chore: youtube -> rutube

--- src/blocks/modules/modal/modal-video/modal-video.js
+++ src/blocks/modules/modal/modal-video/modal-video.js
@@ -8,14 +8,23 @@ const buttonCloseVideoModal = modalVideo.querySelector('.modal__button');
 
 function openVideoModal() {
   modalContent.innerHTML =
-    '<iframe ' +
-    'class="modal__iframe" ' +
-    'src="https://www.youtube.com/embed/_Z5coMfFloc?si=bB9ThMYQoA61cf24&amp;controls=0&amp;autoplay=1&amp;autoplay=1" ' +
-    'title="Красота гор" ' +
-    'allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ' +
-    'referrerpolicy="strict-origin-when-cross-origin" ' +
-    'allowfullscreen>' +
+    '<iframe class="modal__iframe" ' +
+    'src="https://rutube.ru/play/embed/0a6d4dc96711efda47bc8fab55146806/?skinColor=00897b&t=3" ' +
+    'allow="clipboard-write; autoplay" ' +
+    'webkitAllowFullScreen' +
+    'mozallowfullscreen ' +
+    'allowFullScreen>' +
     '</iframe>';
+
+  // '<iframe ' +
+  // 'class="modal__iframe" ' +
+  // 'src="https://www.youtube.com/embed/_Z5coMfFloc?si=bB9ThMYQoA61cf24&amp;controls=0&amp;autoplay=1&amp;autoplay=1" ' +
+  // 'title="Красота гор" ' +
+  // 'allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" ' +
+  // 'referrerpolicy="strict-origin-when-cross-origin" ' +
+  // 'allowfullscreen>' +
+  // '</iframe>';
+
   openModal(modalVideo);
 }
 
